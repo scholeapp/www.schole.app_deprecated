dfd531ab2e5016f3d8db0d733d85fc6d033874eb
N.Watanabe <asterisk37n@gmail.com>
2023-05-17 15:57:40 +0900

    update google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
 import Head from 'next/head'
 
+const GTM_ID = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_CONTAINER_ID || '' 
+
 function MyApp({ Component, pageProps }) {
   return (
     <>
@@ -11,18 +13,20 @@ function MyApp({ Component, pageProps }) {
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-LGX89LF33R"
         ></script>
-
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag() { dataLayer.push(arguments); };
-          gtag('js', new Date());
-          gtag('config', 'G-LGX89LF33R');
-            `,
-          }}
-        />
       </Head>
+      <Script
+        id="gtag-base"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', '${GTM_ID}');
+          `,
+        }}
+      />
       <Component {...pageProps} />
     </>
   );
